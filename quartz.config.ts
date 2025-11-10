4519e664faa98d063e19e0067a111c40fb69e3d9
azazel42 <azazel42@duck.com>
2025-11-09 18:02:11 -0600

    brought changed config datetype

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "azazel420.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
